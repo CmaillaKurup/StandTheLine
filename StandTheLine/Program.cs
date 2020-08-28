0685f838eb3e9c6e5e746d036e55a1378daa4b2f
CmaillaKurup <cami670v@zbc.dk>
2020-08-28 12:46:08 +0200

    First push

---
+++ StandTheLine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StandTheLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
